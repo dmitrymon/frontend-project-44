b3b499c5cda9d8510b9a42008208dba3c31e9900
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2022-12-28 23:38:16 +0300

    add brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,60 +1,7 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import { startGame } from '../src/index.js';
 
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!
-What is the result of the expression?`);
+import { rules, startRound } from '../src/games/game-calc.js';
 
-const operators = ['+', '-', '*'];
-
-const generateRandomOperator = () => Math.floor(Math.random() * 3);
-
-const generateRandomNumber = () => Math.round(Math.random() * 100);
-
-const sumOfNumbers = (x, y) => (x + y);
-
-const differenceOfNumbers = (x, y) => (x - y);
-
-const productOfNumbers = (x, y) => (x * y);
-
-const startRound = () => {
-  const randomNumber1 = generateRandomNumber();
-  const randomNumber2 = generateRandomNumber();
-  const operatorIndex = operators[generateRandomOperator()];
-  let answer;
-  let correctAnswer;
-  if (operatorIndex === 0) {
-    answer = readlineSync.question(`Question: ${randomNumber1} + ${randomNumber2}
-Your answer: `);
-    correctAnswer = sumOfNumbers(randomNumber1, randomNumber2);
-  } else if (operatorIndex === 1) {
-    answer = readlineSync.question(`Question: ${randomNumber1} - ${randomNumber2}
-Your answer: `);
-    correctAnswer = differenceOfNumbers(randomNumber1, randomNumber2);
-  } else if (operatorIndex === 2) {
-    answer = readlineSync.question(`Question: ${randomNumber1} * ${randomNumber2}
-Your answer: `);
-    correctAnswer = productOfNumbers(randomNumber1, randomNumber2);
-  }
-
-  if (answer === correctAnswer) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-  return false;
-};
-
-const startGame = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const isCorrect = startRound();
-    if (!isCorrect) {
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
-
-startRound();
+startGame(rules, startRound);
---
+++ src/games/game-calc.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+
+import { generateRandomNumber } from '../index.js';
+
+const operators = ['+', '-', '*'];
+
+const generateRandomOperator = () => Math.floor(Math.random() * 3);
+
+const sumOfNumbers = (x, y) => (x + y);
+
+const differenceOfNumbers = (x, y) => (x - y);
+
+const productOfNumbers = (x, y) => (x * y);
+
+const rules = 'What is the result of the expression?';
+
+const startRound = () => {
+  const randomNumber1 = generateRandomNumber();
+  const randomNumber2 = generateRandomNumber();
+  const operatorIndex = generateRandomOperator();
+  let userAnswer;
+  let correctAnswer;
+  if (operatorIndex === 0) {
+    userAnswer = readlineSync.question(`Question: ${randomNumber1} ${operators[0]} ${randomNumber2}
+Your answer: `);
+    correctAnswer = sumOfNumbers(randomNumber1, randomNumber2).toString();
+  } else if (operatorIndex === 1) {
+    userAnswer = readlineSync.question(`Question: ${randomNumber1} ${operators[1]} ${randomNumber2}
+Your answer: `);
+    correctAnswer = differenceOfNumbers(randomNumber1, randomNumber2).toString();
+  } else if (operatorIndex === 2) {
+    userAnswer = readlineSync.question(`Question: ${randomNumber1} ${operators[2]} ${randomNumber2}
+Your answer: `);
+    correctAnswer = productOfNumbers(randomNumber1, randomNumber2).toString();
+  }
+  return [userAnswer, correctAnswer];
+};
+
+export { rules, startRound };
