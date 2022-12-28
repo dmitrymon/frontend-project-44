87d0af1accce2bc7f5c0bd391cef3ea9f12d0137
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2022-12-28 23:35:16 +0300

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import readlineSync from 'readline-sync';
+
+const generateRandomNumber = () => Math.round(Math.random() * 100);
+
+const startGame = (rules, startRound) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rules);
+  for (let i = 0; i < 3; i += 1) {
+    const [userAnswer, correctAnswer] = startRound();
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export { generateRandomNumber, startGame };
