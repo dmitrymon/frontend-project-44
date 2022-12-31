f437a4a5142bbd7fd76afa6a7c23d672a6481eb1
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2022-12-31 17:12:26 +0300

    add src/games/game-prime.js

---
+++ src/games/game-prime.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+
+import { generateRandomNumber } from '../index.js';
+
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return 'no';
+  }
+  const dividers = [1];
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      dividers.push(i);
+    }
+  }
+  dividers.push(number);
+  return (dividers[1] === number ? 'yes' : 'no');
+};
+
+const startRound = () => {
+  const randomNumber = generateRandomNumber();
+  const userAnswer = readlineSync.question(`Question: ${randomNumber}
+Your answer: `);
+  const correctAnswer = isPrime(randomNumber);
+  return [userAnswer, correctAnswer];
+};
+
+export { rules, startRound };
