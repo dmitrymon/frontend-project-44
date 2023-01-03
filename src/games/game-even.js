f9966d70c5c95fb3a75bc9b3e26757bfd3871d9e
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-01-03 16:58:12 +0300

    fix isEven() in src/games/game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -6,13 +6,13 @@ import startGame from '../index.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+const isEven = (number) => number % 2 === 0;
 
 const startRound = () => {
   const randomNumber = generateRandomNumber();
   const userAnswer = readlineSync.question(`Question: ${randomNumber}
 Your answer: `);
-  const correctAnswer = isEven(randomNumber);
+  const correctAnswer = (isEven(randomNumber) ? 'yes' : 'no');
   return [userAnswer, correctAnswer];
 };
 
