47f6cc20c7a3730a0fc0b8a53b7b8b41f3f221fb
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-01-09 20:46:26 +0300

    fix src/games/game-progression.js

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -6,28 +6,24 @@ import startGame from '../index.js';
 
 const rules = 'What number is missing in the progression?';
 
-let progressionSize;
-
-let progression = [];
-
 const generateProgression = () => {
   const minSize = 5;
   const maxSize = 10;
-  progressionSize = generateRandomNumber(maxSize, minSize);
+  const progressionSize = generateRandomNumber(maxSize, minSize);
   const firstNumber = generateRandomNumber();
-  progression = [firstNumber];
+  const progression = [firstNumber];
   const stepSizeLimit = 10;
-  const stepSize = generateRandomNumber(stepSizeLimit);
+  const stepSize = generateRandomNumber(stepSizeLimit, 1);
   let nextNumber = firstNumber + stepSize;
   for (let i = 1; i < progressionSize; i += 1) {
     progression.push(nextNumber);
     nextNumber += stepSize;
   }
-  return progression;
+  return [progressionSize, progression];
 };
 
 const startRound = () => {
-  progression = generateProgression();
+  const [progressionSize, progression] = generateProgression();
   const gapIndexLimit = progressionSize;
   const gapIndex = generateRandomNumber(gapIndexLimit);
   const correctAnswer = progression[gapIndex].toString();
