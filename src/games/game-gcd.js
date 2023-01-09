80e73c874ade1e2487a1ba2cff78d3e1ee221c93
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-01-09 21:09:08 +0300

    fix magic numbers in src/games/game-gcd.js

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -27,8 +27,10 @@ const greatestCommonDivisor = (number1, number2) => {
 };
 
 const startRound = () => {
-  const randomNumber1 = generateRandomNumber(100, 1);
-  const randomNumber2 = generateRandomNumber(100, 1);
+  const maxNum = 100;
+  const minNum = 1;
+  const randomNumber1 = generateRandomNumber(maxNum, minNum);
+  const randomNumber2 = generateRandomNumber(maxNum, minNum);
   const userAnswer = readlineSync.question(`Question: ${randomNumber1} ${randomNumber2}
 Your answer: `);
   const correctAnswer = greatestCommonDivisor(randomNumber1, randomNumber2);
