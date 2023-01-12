9651129c8f628bd2214b32594d5ad7df9f5559fb
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-01-13 00:17:17 +0300

    fix src/games/game-calc.js

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -6,27 +6,33 @@ import startGame from '../index.js';
 
 const rules = 'What is the result of the expression?';
 
+const operators = ['+', '-', '*'];
+
+const calculate = (num1, num2, operator) => {
+  let answer;
+  switch (operator) {
+    case '+':
+      answer = `${num1 + num2}`;
+      break;
+    case '-':
+      answer = `${num1 - num2}`;
+      break;
+    case '*':
+      answer = `${num1 * num2}`;
+      break;
+    default:
+      throw new Error(`Unknown operator: '${operator}'`);
+  }
+  return answer;
+};
+
 const startRound = () => {
   const randomNumber1 = generateRandomNumber();
   const randomNumber2 = generateRandomNumber();
-  const operators = ['+', '-', '*'];
-  const lastOperatorIndex = 2;
-  const operator = operators[generateRandomNumber(lastOperatorIndex)];
+  const operator = operators[generateRandomNumber(operators.length - 1)];
   const userAnswer = readlineSync.question(`Question: ${randomNumber1} ${operator} ${randomNumber2}
 Your answer: `);
-  let correctAnswer;
-  switch (operator) {
-    case '+':
-      correctAnswer = `${randomNumber1 + randomNumber2}`;
-      break;
-    case '-':
-      correctAnswer = `${randomNumber1 - randomNumber2}`;
-      break;
-    case '*':
-      correctAnswer = `${randomNumber1 * randomNumber2}`;
-      break;
-    default:
-  }
+  const correctAnswer = calculate(randomNumber1, randomNumber2, operator);
   return [userAnswer, correctAnswer];
 };
 
