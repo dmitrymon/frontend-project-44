9feac19e9dc088ef91cbdb506db8fb80dd922c9a
Dmitry Sergeev <dmitrymonsergeev@gmail.com>
2023-01-14 15:42:15 +0300

    fix isPrime in src/games/game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -8,16 +8,14 @@ const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
   if (number < 2) {
-    return 'no';
+    return false;
   }
-  const dividers = [1];
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
-      dividers.push(i);
+      return false;
     }
   }
-  dividers.push(number);
-  return dividers[1] === number;
+  return true;
 };
 
 const startRound = () => {
